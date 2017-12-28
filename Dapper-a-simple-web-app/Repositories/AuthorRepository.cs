99dfc3c92f0a43fb8a7bf467e5971d0abe694c79
Gareth Elms <garethmarkelms@gmail.com>
2017-12-28 13:41:05 +0000

    Great, that's many-to-many done at least with a single many list eg; author -> articles. Next is Article -> tags. Going for a run now.

--- Dapper-a-simple-web-app/Repositories/AuthorRepository.cs
+++ Dapper-a-simple-web-app/Repositories/AuthorRepository.cs
@@ -19,22 +19,28 @@ namespace Dapper_SimpleWebApp
 
 		public Author RetrieveById(int authorId)
 		{
-			//var result = _connection.Query<Author>("select * from author where id=@id", new {Id = authorId}).SingleOrDefault();
 			var authors = new Dictionary<long, Author>();
 
 			var result = _connection.Query<Author, Article, Author>(
-				"select * from author  join article on article.authorid=author.id where author.id=@Id",
+				"select * from author left join article on article.authorid=author.id where author.id=@Id",
 				(author, article) => {
 					if(!authors.ContainsKey(author.Id))
 					{
 						authors[author.Id] = author;
-						author.Articles = new List<Article>();
+						if(article != null)
+						{
+							author.Articles = new List<Article>();
+						}
 					}
 					else
 					{
 						author = authors[author.Id];
 					}
-					author.Articles.Add(article); return author;
+					if(article != null)
+					{
+						author.Articles.Add(article);
+					}
+					return author;
 				},
 				new {Id = authorId});
 
@@ -55,8 +61,37 @@ namespace Dapper_SimpleWebApp
 
 		public List<Author> FetchAll()
 		{
-			var authors = _connection.Query<Author>("select * from author order by name").ToList();
-			return authors;
+			//var authors = _connection.Query<Author>("select * from author order by name").ToList();
+
+			var authors = new Dictionary<long, Author>();
+
+			var result = _connection.Query<Author, Article, Author>(
+				"select * from author left join article on article.authorid=author.id order by author.name",
+				(author, article) => {
+					if(!authors.ContainsKey(author.Id))
+					{
+						authors[author.Id] = author;
+						if(article != null)
+						{
+							author.Articles = new List<Article>();
+						}
+					}
+					else
+					{
+						author = authors[author.Id];
+					}
+					if(article != null)
+					{
+						author.Articles.Add(article);
+					}
+					return author;
+				});
+
+			if(authors.Count > 0)
+			{
+				return authors.Values.ToList();
+			}
+			return null;
 		}
 
 		public bool Save(Author author)
